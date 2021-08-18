58d46263d7e65c5ce2c08a68c152fd59cf162c8a
matichewer <schwerdt.matias@gmail.com>
2021-08-18 20:11:09 -0300

    mensajes de telegram removidos, log mejorado

--- spliktv.sh
+++ spliktv.sh
@@ -3,38 +3,52 @@
 
 ####################################### CONFIG PATH #######################################
 
-THIS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#THIS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Load the config to use the telegram bot
-. "${THIS_PATH}/TelegramBashBotsFramework/tbb.sh"
+#. "${THIS_PATH}/TelegramBashBotsFramework/tbb.sh"
 
 # Logs
-LOG="${THIS_PATH}/log.txt"
+#LOG="${THIS_PATH}/log.txt"
 
 ###########################################################################################
 
+PATH_LOG="log.txt"
 
+saveLog(){
+    # Guardo logs
+    echo -e "$(date '+%Y/%m/%d%t%H:%M:%S')\t${STATUS}" >> ${PATH_LOG}
+    echo "${STATUS}"
+}
 
 # Obtengo link personal de activacion
-LINK=$(curl --silent --include                                                 \
+LINK=$(curl                                                                    \
+            --silent                                                           \
+            --include                                                          \
             'https://app.spliktv.xyz/activar'                                  \
-            | grep "ocation"                                                  \
+            | grep "ocation"                                                   \
             | cut --delimiter=' ' --fields=2                                   \
             | tr -d " \t\n\r") 
 
 # Si curl() NO retorna 0, entonces hubo un error en la generacion del link
 if [ $? -ne "0" ]; then 
-    STATUS="no se pudo generar el link"
+    STATUS="Error: no se pudo generar el link"
+    saveLog
+    exit 1
 fi
 
 
 # Intento activar la app con el link generado previamente
-CURL_OUTPUT=$(curl --silent ${LINK}                                            \
+CURL_OUTPUT=$(curl                                                             \
+                --silent                                                       \
+                "${LINK}"                                                      \
                 --data-raw 'submite=Pulsa+aqu%C3%AD+para+activar')
 
 # Si curl() NO retorna 0, entonces hubo un error en la conexion
 if [ $? -ne "0" ]; then 
-    STATUS="link generado, pero no se pudo presionar el boton de activacion"
+    STATUS="Error: link generado, pero no se pudo presionar el boton de activacion"
+    saveLog
+    exit 1
 else
 
     # Busco si realmente se ha activado
@@ -45,17 +59,19 @@ else
         # Busco si ya habia sido activado previamente  
         grep -q "Ya activaste" <<< "${CURL_OUTPUT}"
         if [ $? -ne "0" ]; then
-            STATUS="conexion realizada, pero no se pudo activar, ni estaba activado de antes"
+            STATUS="Error: conexion realizada, pero no se pudo activar ni tampoco estaba activado"
+            saveLog
+            exit 1
         else
-            STATUS="no requeria activacion"
+            STATUS="OK: no requeria activacion, ya lo estaba"
         fi
     else       
         # En éste punto ya se tiene que haber activado si o si 
-        STATUS="activado"        
-        sendMessage "text:SplikTV: ${STATUS}" > /dev/null
+        STATUS="OK: activado"        
+        #sendMessage "text:SplikTV: ${STATUS}" > /dev/null
     fi
 fi
 
-# Guardo logs
-echo -e "$(date '+%Y/%m/%d%t%H:%M:%S')\t${STATUS}" >> ${LOG}
-echo ${STATUS}
+# Si la ejecucion llega a éste punto, entonces no hubo ningun error
+saveLog
+
