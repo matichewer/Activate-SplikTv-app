6057eff3138d590d7b6f9332236e157200e4ffff
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-06-27 23:03:05 -0300

    curl mas robusto, falta testear

--- spliktv.sh
+++ spliktv.sh
@@ -9,11 +9,37 @@ saveLog(){
 }
 
 
+: '
 # Intento activar la app
 CURL_OUTPUT=$(curl                                                             \
                 --silent                                                       \
                 "${LINK}"                                                      \
                 --data-raw 'submite=Pulsa+aqu%C3%AD+para+activar')
+'
+
+CURL_OUTPUT=$(curl 'https://sv.spliktv.xyz/activar' \
+                --silent \
+                -H 'authority: sv.spliktv.xyz' \
+                -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+                -H 'accept-language: en-US,en;q=0.9,es;q=0.8,la;q=0.7' \
+                -H 'content-type: application/x-www-form-urlencoded' \-H 'dnt: 1' \
+                -H 'origin: https://sv.spliktv.xyz' \
+                -H 'referer: https://sv.spliktv.xyz/activar' \
+                -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"' \
+                -H 'sec-ch-ua-mobile: ?0' \
+                -H 'sec-ch-ua-platform: "Linux"' \
+                -H 'sec-fetch-dest: document' \
+                -H 'sec-fetch-mode: navigate' \
+                -H 'sec-fetch-site: same-origin' \
+                -H 'sec-fetch-user: ?1' \
+                -H 'upgrade-insecure-requests: 1' \
+                -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36' \
+                --data-raw 'si=Pulsa+aqu%C3%AD+para+activar' \
+                --compressed
+            )
+
+
+
 
 # Si curl() NO retorna 0, entonces hubo un error en la conexion
 if [ $? -ne "0" ]; then 
